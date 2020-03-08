31028a5eb4360b52527eb24490de355ec5b4b5de
lsh58 <60163978+lsh58@users.noreply.github.com>
2020-03-08 14:33:15 +0900

    porfolio work

---
+++ 스토리보드수행과제/완성/index/js/common.js
@@ -0,0 +1,13 @@
+// var imgArray = new Array();
+// imgArray[0] = "url(/index/img/backimg_01.jpg) no-repeat center 30% / cover";
+// imgArray[1] = "url(/index/img/backimg_02.jpg) no-repeat center 100% / cover";
+// imgArray[2] = "url(/index/img/backimg_03.jpg) no-repeat center 100% / cover";
+
+// function bgchanger() {
+//     var imgNum = Math.round(Math.random()*2);
+//     var background = document.getElementById("background");
+//     background.style.background = imgArray[imgNum];
+//     setTimeout("bgchanger()", 5000);
+//     console.group(imgNum);
+// }
+// bgchanger();
