134dd082920a700548c55d82188925fb37d7d7ba
lsh58 <tmdgusdl58@gmail.com>
2020-03-09 15:58:58 +0900

    portfolio work

--- 스토리보드수행과제/완성/index/js/script.js
+++ 스토리보드수행과제/완성/index/js/script.js
@@ -1,5 +1,34 @@
-var slide = document.getElementById("slide");
+// D-day 출력
 
-slide.addEventListener("mouseover",function(){
-    slide.className = "stop";
-})
+var dday = document.querySelector(".top strong");
+var now = new Date();
+var day = new Date("may 16,2020");
+var gap = now.getTime() - day.getTime();
+gap = Math.floor(gap / (1000 * 60 * 60 * 24));
+dday.innerHTML = "D" + gap;
+
+// D-day 출력 end
+
+//배경변경
+var bg = document.getElementsByTagName("header");
+var imgarr = new Array();
+for (var i = 0; i < 3; i++) {
+    imgarr[i] = "url(img/backimg_0" + (i + 1) + ".jpg)";
+}
+
+var i = 0;
+function showSlides() {
+    if (i > 2) {
+        i = 0;
+    }
+    if (i == 0) {
+        bg[0].style.backgroundPositionY = "80%";
+    } else {
+        bg[0].style.backgroundPositionY = "200%";
+    }
+    bg[0].style.backgroundImage = imgarr[i];
+    i++;
+}
+setInterval(showSlides, 3000);
+
+//배경변경 end
