fad99f9066ac2454436a787af869d54cb6b4d6aa
lsh58 <60163978+lsh58@users.noreply.github.com>
2020-03-17 07:07:20 +0900

    css

--- 스토리보드수행과제/완성/sub03/js/sub03.js
+++ 스토리보드수행과제/완성/sub03/js/sub03.js
@@ -1,6 +1,4 @@
 
-
-
 var ticket_nav = document.querySelectorAll('.ticket_nav a');
 for (var i = 0; i < ticket_nav.length; i++) {
     ticket_nav[i].addEventListener('click', function (e) {
@@ -13,38 +11,40 @@ for (var i = 0; i < ticket_nav.length; i++) {
 }
 
 
-var win_height = window.innerHeight; /* 윈도우의 높이 */
+function getCurrentScrollPercentage(){
+    return (window.scrollY + window.innerHeight) / document.body.clientHeight * 100}
+
 var li = document.querySelectorAll('li');
 var ul = document.querySelector('ul');
 
 window.addEventListener("scroll", function () {
-    var scrollY = window.scrollY; /* 스크롤 값 */
-    if (scrollY < 10) {
+    var scrollY = getCurrentScrollPercentage();
+    if (scrollY < 40) {
         for (var i = 0; i < li.length; i++) {
             li[i].classList.remove('active');
             ul.classList.remove('active');
         }
     }
-    if (scrollY >= 10) {
+    if (scrollY >= 30) {
         ul.classList.add('active');
         li[0].classList.add("active");
         li[1].classList.add("active");
         li[2].classList.add("active");
         li[3].classList.add("active");
     }
-    if (scrollY >= 100) {
+    if (scrollY >= 40) {
         li[4].classList.add("active");
         li[5].classList.add("active");
     }
-    if (scrollY >= 200) {
+    if (scrollY >= 50) {
         li[6].classList.add("active");
         li[7].classList.add("active");
     }
-    if (scrollY >= 300) {
+    if (scrollY >= 60) {
         li[8].classList.add("active");
         li[9].classList.add("active");
     }
-    if (scrollY >= 400) {
+    if (scrollY >= 70) {
         li[10].classList.add("active");
         li[11].classList.add("active");
     }
