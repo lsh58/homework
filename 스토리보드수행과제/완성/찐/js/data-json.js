aa25fb2b12fb4a94d510ac0fb0f87de1f9048f90
lsh58 <tmdgusdl58@gmail.com>
2020-03-19 20:26:02 +0900

    .

--- 스토리보드수행과제/완성/찐/js/data-json.js
+++ 스토리보드수행과제/완성/찐/js/data-json.js
@@ -11,19 +11,60 @@ xhr.onload = function () {                       // When readystate changes
 
   // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
   var firstContent = '';
+
+
   for (var i = 0; i < responseObject.first.length; i++) { // Loop through object
-    firstContent += '<a href="" style = "background: url(' + responseObject.first[i].url + ') no-repeat center/ cover;"><i class="fas fa-search"></i></a>';
+    firstContent += '<a href="" style = "background: url(' + responseObject.first[i].url + ') no-repeat center/ cover;"><i class="fas fa-search"></i></a>'
   }
 
   // Update the page with the new content
   document.getElementById('first').innerHTML = firstContent;
 
+  var wrapper = document.querySelectorAll('.img-wrapper');
+  var wrapper_first = wrapper[0].querySelectorAll('.img-sub a');
+  var content_first = wrapper[0].querySelector('.img-main .content');
+  var h3_first = wrapper[0].querySelector('.img-main .content #h3_f');
+  var text_first = wrapper[0].querySelector('.img-main .content #text_f');
+  var fb_first = wrapper[0].querySelector('.img-main .content #fb_f');
+  var insta_first = wrapper[0].querySelector('.img-main .content #insta_f');
+
+  for (let i = 0; i < wrapper_first.length; i++) {
+    wrapper_first[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log(wrapper_first);
+      h3_first.innerHTML = responseObject.first[i].name;
+      text_first.innerHTML = responseObject.first[i].text;
+      fb_first.innerHTML = responseObject.first[i].facebook;
+      insta_first.innerHTML = responseObject.first[i].instagram;
+    })
+  }
+
   //}
+
   var secondContent = '';
   for (var i = 0; i < responseObject.second.length; i++) { // Loop through object
     secondContent += '<a href="" style = "background: url(' + responseObject.second[i].url + ') no-repeat center/ cover;"><i class="fas fa-search"></i></a>';
   }
   document.getElementById('second').innerHTML = secondContent;
+
+  var wrapper_second = wrapper[1].querySelectorAll('.img-sub a');
+  var content_second = wrapper[1].querySelector('.img-main .content');
+  var h3_second = wrapper[1].querySelector('.img-main .content #h3_s');
+  var text_second = wrapper[1].querySelector('.img-main .content #text_s');
+  var fb_second = wrapper[1].querySelector('.img-main .content #fb_s');
+  var insta_second = wrapper[1].querySelector('.img-main .content #insta_s');
+
+  for (let i = 0; i < wrapper_second.length; i++) {
+    wrapper_second[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log(wrapper_second);
+      h3_second.innerHTML = responseObject.second[i].name;
+      text_second.innerHTML = responseObject.second[i].text;
+      fb_second.innerHTML = responseObject.second[i].facebook;
+      insta_second.innerHTML = responseObject.second[i].instagram;
+    })
+  }
+
 };
 
 xhr.open('GET', 'data.json', true);        // Prepare the request
--- 스토리보드수행과제/완성/찐/js/sub02.js
+++ 스토리보드수행과제/완성/찐/js/sub02.js
@@ -9,39 +9,37 @@ window.addEventListener('load', function () {
     // console.log(document.querySelector(".img-sub"))
 
     for (var i = 0; i < wrapper_first.length; i++) {
-        wrapper_first[i].addEventListener('mouseenter', function (e) {
+        wrapper_first[i].addEventListener('click', function (e) {
             e.preventDefault();
             var imgname = this.style.backgroundImage;
             mainimg[0].style = "background:" + imgname + " no-repeat center / cover;";
+            mainimg[0].addEventListener('mouseover', function () {
+                content_first.style.opacity = 1;
+            })
+            mainimg[0].addEventListener('mouseleave', function () {
+                content_first.style.opacity = 0;
+            })
+        })
 
-        })
-        wrapper_first[i].addEventListener('click', function (e) {
-            e.preventDefault();
-            content_first.style.opacity = 1;
-        })
-
-        wrapper_first[i].addEventListener('mouseleave', function (e) {
-            var imgname = this.style.backgroundImage;
-            content_first.style.opacity = 0;
-            mainimg[0].style = "background: url('../img/lineup/lineup01.jpg') no-repeat center/ cover;";
-        })
     }
+    // wrapper_second[i].addEventListener('mouseleave', function (e) {
+    //     content_second.style.opacity = 0;
+    //     mainimg[1].style = "background: url('../img/lineup/lineup01.jpg') no-repeat center/ cover;";
+    // })
 
     for (var i = 0; i < wrapper_second.length; i++) {
-        wrapper_second[i].addEventListener('mouseenter', function (e) {
+        wrapper_second[i].addEventListener('click', function (e) {
             e.preventDefault();
             var imgname = this.style.backgroundImage;
             mainimg[1].style = "background:" + imgname + " no-repeat center / cover;";
+            mainimg[1].addEventListener('mouseover', function () {
+                content_second.style.opacity = 1;
+            })
+            mainimg[1].addEventListener('mouseleave', function () {
+                content_second.style.opacity = 0;
+            })
         })
-        wrapper_second[i].addEventListener('click', function (e) {
-            e.preventDefault();
-            content_second.style.opacity = 1;
-        })
-        wrapper_second[i].addEventListener('mouseleave', function (e) {
-            var imgname = this.style.backgroundImage;
-            content_second.style.opacity = 0;
-            mainimg[1].style = "background: url('../img/lineup/lineup01.jpg') no-repeat center/ cover;";
-        })
+
     }
 });
 //json이용해서 할것
